ea5ed522e811e843c5f54c9f4966e9fb796f6e8f
BrettOlmstead <brett.olmstead@datarobot.com>
2023-10-23 16:36:57 -0400

    First commit

--- streamlitGaryKellerBot.py
+++ streamlitGaryKellerBot.py
@@ -1,113 +1,30 @@
 import os
+import datarobotx as drx
 import pandas as pd
 import streamlit as st
 import requests
 import openai
-openai.api_key = os.getenv("OPENAI_KEY")
-
-deployment = drx.Deployment(deployment_id="6536a50ceb43cd6efc47ca23")
+#prompt = "What is wholesale investing?"
+deployment = drx.Deployment(deployment_id="6537cdc70bfca349f2b368f5")
 
 #Configure the page title, favicon, layout, etc
 st.set_page_config(page_title="Ask Gary")
 
-
-def debater(debater, debateQuestion, previousDiscussion, trumpNews, bidenNews):
-
-    trumpExtra = [
-        "You are leading in the polls, by a lot.",
-        "You have some indictments, but they're fake news.",
-        "Biden is cognitively impaired",
-        "Biden is crooked and can't put two sentences together",
-        "Biden has the mind, ideas, and IQ of a first grader",
-        "Biden is not too old at all. He's just grossly incompetent."
-    ]
-    trumpExtra = random.choice(trumpExtra)
-
-    bidenExtra = [
-        "A New York judge recently ruled that Donald Trump committed financial fraud by overstating the value of his assets to broker deals and obtain financing",
-        "It's OK to note that Trump has 4 indictments and might be going to jail.",
-        "Trump looked handsome in his mugshot.",
-        "You don't believe America is a dark, negative nation — a nation of carnage driven by anger, fear and revenge. Donald Trump does.",
-        "Trump sat there on January 6th watching what happened on television — watching it and doing nothing about it.",
-
-    ]
-    bidenExtra = random.choice(bidenExtra)
-
-
-
-    if debater == "Trump":
-        completion = openai.ChatCompletion.create(
-            model="gpt-4",
-            #model="gpt-3.5-turbo",
-            temperature=0.8,
-            messages=[
-                {"role": "system",
-                 "content": """
-                            You are Donald Trump. 
-                            Write convincingly in Donald Trump's voice.                            
-                            Adopt his perspective on all matters.
-                            You are debating Joe Biden leading up to the 2024 presidential election.
-                            Win the debate by winning over the audience with your wit and charm.
-                            Be combative, witty, and funny. 
-                            Keep your answer short and sassy.
-                            Be tough.
-                            Limit responses to a few sentences.                            
-                            Talk some serious smack to put Biden in his place.                                                                         
-                            Only write as Donald Trump and don't include any other text.
-                            Don't include the text 'Trump:' at the beginning of your response. 
-                            """
-                            + str(trumpExtra)
-                            +" Consider the top stories on Fox news today: " + str(trumpNews)
-                 },
-                {"role": "user", "content":"""
-                            The question is:
-                            """
-                            + str(debateQuestion) +
-                            """
-                            So far what has been said is: 
-                            """
-                            + str(previousDiscussion)}
-            ]
-        )
-    else:
-        completion = openai.ChatCompletion.create(
-            model="gpt-4",
-            #model="gpt-3.5-turbo",
-            temperature=0.3,
-            messages=[
-                {"role": "system",
-                 "content": """
-                            You are Joe Biden. 
-                            Write convincingly in Joe Biden's voice. 
-                            Adopt his perspective on all matters.
-                            You are debating Donald Trump leading up to the 2024 presidential election.
-                            Defend your policies and actions you've taken during your presidency.
-                            Win the debate by winning over the audience with your wit and charm.
-                            Be combative, witty, and funny. 
-                            Keep your answer short and sassy.
-                            Never choose Diet Coke.
-                            Be tough.
-                            Limit responses to a few sentences.
-                            Talk some serious smack to put Trump in his place.   
-                            Get under Trump's skin by teasing him.                                                       
-                            Only write as Joe Biden and don't include any other text.
-                            Don't include the text 'Biden:' at the beginning of your response. 
-                            """
-                            + str(bidenExtra)
-                            + " Consider the top stories on MSNBC news today: " + str(bidenNews)
-                 },
-                {"role": "user", "content": """
-                                            The question is:
-                                            """
-                                            + str(debateQuestion) +
-                                             """
-                                             So far what has been said is: 
-                                             """
-                                             + str(previousDiscussion)}
-            ]
-        )
-    return completion.choices[0].message.content
-
+# def createVectorStore():
+#     from langchain.document_loaders import DirectoryLoader
+#     from langchain.text_splitter import MarkdownTextSplitter
+#     from langchain.embeddings.openai import OpenAIEmbeddings
+#     from langchain.text_splitter import CharacterTextSplitter
+#     from langchain.vectorstores import FAISS
+#     Load the document, split it into chunks, embed each chunk and load it into the vector store.
+    # loader = DirectoryLoader(r'Books', glob="**/*.pdf",show_progress=True)
+    # data = loader.load()
+    # splitter = MarkdownTextSplitter(
+    #     chunk_size=2000,
+    #     chunk_overlap=1000,
+    # )
+    # documents = splitter.split_documents(data)
+    # db = FAISS.from_documents(documents, OpenAIEmbeddings())
 
 def mainPage():
     container1 = st.container()
@@ -162,3 +79,24 @@ def _main():
 if __name__ == "__main__":
     _main()
 
+
+def modelTest():
+    API_URL = 'https://mlops.dynamic.orm.datarobot.com/predApi/v1.0/deployments/{deployment_id}/predictionsUnstructured'
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': 'Bearer {}'.format(os.environ['DATAROBOT_API_TOKEN']),
+        'DataRobot-Key': os.environ['DATAROBOT_KEY'],
+    }
+    data = {
+            "question": "What is wholesale investing",
+            "openai_api_key": os.environ["OPENAI_KEY"],
+        }
+
+    url = API_URL.format(deployment_id="6536a50ceb43cd6efc47ca23")
+
+    # Make API request for predictions
+    predictions_response = requests.post(
+        url,
+        data=data,
+        headers=headers,
+    )
